9919bbdd349b28fb773ec433ecc704f6f6df892c
José Carlos Peñuelas Armenta <jose18_carl@hotmail.com>
2019-03-25 15:40:36 -0600

    simple index.html

--- DevOpsPractice/server.js
+++ DevOpsPractice/server.js
@@ -1,14 +1,16 @@
-const http = require('http');
-
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 const hostname = '127.0.0.1';
 const port = 8000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Werld\n');
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.use('/', router);
+app.listen(process.env.port || port);
+
+console.log(`Server running at http://${hostname}:${port}/`);
